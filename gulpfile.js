ea6c6f8dfc56a9139fe58172ac9b2fe80c7c420c
supernoir <kawohl.bentodesign@gmail.com>
2017-06-05 01:57:07 +0200

    adds css minification and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
 
 //sass
+
+gulp.task('minify-css', function() {
+  return gulp
+    .src('./src/**/*.css')
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('dist'));
+});
+
 gulp.task('sass', function() {
   gulp
     .src(['styles/*.scss', 'styles/**/*.scss'])
-    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('css/'));
 });
 
@@ -17,3 +29,8 @@ gulp.task('sass:watch', function() {
 gulp.task('default', function() {
   gulp.start('sass');
 });
+
+gulp.task('production', function() {
+  gulp.start('minify-css');
+});
+
